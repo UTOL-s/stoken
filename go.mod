c03f630f6a3c34a32514287bd15897fb96cf838a
Jim Dela Cruz <dcruz.jim21@gmail.com>
2025-06-23 21:05:32 +0800

    feature: add middleware

--- go.mod
+++ go.mod
@@ -14,7 +14,11 @@ require (
 	github.com/h2non/parth v0.0.0-20190131123155-b4df798d6542 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/labstack/echo/v4 v4.13.4 // indirect
+	github.com/labstack/gommon v0.4.2 // indirect
 	github.com/magiconair/properties v1.8.7 // indirect
+	github.com/mattn/go-colorable v0.1.14 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/nyaruka/phonenumbers v1.6.3 // indirect
 	github.com/pelletier/go-toml/v2 v2.2.2 // indirect
@@ -31,14 +35,17 @@ require (
 	github.com/subosito/gotenv v1.6.0 // indirect
 	github.com/supertokens/supertokens-golang v0.25.1 // indirect
 	github.com/twilio/twilio-go v0.26.0 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasttemplate v1.2.2 // indirect
 	go.uber.org/dig v1.19.0 // indirect
 	go.uber.org/fx v1.24.0 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
 	go.uber.org/zap v1.27.0 // indirect
+	golang.org/x/crypto v0.38.0 // indirect
 	golang.org/x/exp v0.0.0-20250305212735-054e65f0b394 // indirect
-	golang.org/x/net v0.23.0 // indirect
-	golang.org/x/sys v0.18.0 // indirect
-	golang.org/x/text v0.23.0 // indirect
+	golang.org/x/net v0.40.0 // indirect
+	golang.org/x/sys v0.33.0 // indirect
+	golang.org/x/text v0.25.0 // indirect
 	google.golang.org/protobuf v1.36.5 // indirect
 	gopkg.in/alexcesaro/quotedprintable.v3 v3.0.0-20150716171945-2caba252f4dc // indirect
 	gopkg.in/gomail.v2 v2.0.0-20160411212932-81ebce5c23df // indirect
---
+++ pkg/middleware/cors.go
@@ -0,0 +1,66 @@
+package middleware
+
+import (
+	"net/http"
+	"strings"
+	
+	"github.com/ankorstore/yokai/config"
+	"github.com/labstack/echo/v4"
+	"github.com/supertokens/supertokens-golang/supertokens"
+)
+
+type CorsMiddleware struct {
+	config *config.Config
+}
+
+func NewCorsMiddleware(config *config.Config) *CorsMiddleware {
+	return &CorsMiddleware{config}
+}
+
+func (m *CorsMiddleware) Handle(next http.Handler) echo.MiddlewareFunc {
+	return func(hf echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			allowedOrigins := []string{
+				"http://localhost:3000",
+				"http://localhost:8000",
+				"https://www.utol.com",
+				"https://www.utol.com.ph",
+				"https://portal-admin-v2.utol.com.ph",
+				"https://staging-landing-page.utol.com.ph",
+				"https://admin-staging-portal.utol.ph",
+				"https://staging-admin-v2.utol.com.ph",
+				"https://admin-portal.utol.com.ph",
+				"https://staging-admin-portal.utol.com.ph",
+				"https://staging-landing-v2.utol.com.ph",
+				"https://accounting-admin.utol.com.ph",
+				"https://staging-accounting-admin.utol.com.ph",
+			}
+			origin := c.Request().Header.Get("Origin")
+			
+			isAllowedOrigin := false
+			for _, allowedOrigin := range allowedOrigins {
+				if origin == allowedOrigin {
+					isAllowedOrigin = true
+					break
+				}
+			}
+			
+			if isAllowedOrigin {
+				c.Response().Header().Set("Access-Control-Allow-Origin", origin)
+				c.Response().Header().Set("Access-Control-Allow-Credentials", "true")
+			}
+			
+			if c.Request().Method == "OPTIONS" {
+				c.Response().Header().Set("Access-Control-Allow-Headers", strings.Join(append([]string{"Content-Type"}, supertokens.GetAllCORSHeaders()...), ","))
+				c.Response().Header().Set("Access-Control-Allow-Methods", "*")
+				_, err := c.Response().Write([]byte(""))
+				if err != nil {
+					return err
+				}
+				return nil
+			} else {
+				return hf(c)
+			}
+		}
+	}
+}
