29ca5ed4bb8c610bfad6c38b13ed9e4f7120a5f8
Jim Dela Cruz <dcruz.jim21@gmail.com>
2025-06-25 18:51:18 +0800

    fix: initialize token

--- module.go
+++ module.go
@@ -1,9 +1,11 @@
 package stoken
 
 import (
-	"context"
-	
 	"github.com/ankorstore/yokai/config"
+	"github.com/supertokens/supertokens-golang/ingredients/emaildelivery"
+	"github.com/supertokens/supertokens-golang/recipe/passwordless"
+	"github.com/supertokens/supertokens-golang/recipe/passwordless/plessmodels"
+	"github.com/supertokens/supertokens-golang/supertokens"
 	"go.uber.org/fx"
 )
 
@@ -12,7 +14,7 @@ const ModuleName = "stoken"
 var FxSTokenModule = fx.Module(
 	ModuleName,
 	fx.Provide(
-		fx.Annotate(NewDefaultTokenClientFactory, fx.As(new(TokenClientFactory))),
+		//fx.Annotate(NewDefaultTokenClientFactory, fx.As(new(TokenClientFactory))),
 		TokenInitialize,
 	),
 	fx.Invoke(func(*FxTokenClient) {}),
@@ -22,7 +24,7 @@ type FxTokenClientParam struct {
 	fx.In
 	Lifecycle fx.Lifecycle
 	Config    *config.Config
-	Factory   TokenClientFactory
+	//Factory   TokenClientFactory
 }
 
 type FxTokenClient struct {
@@ -30,20 +32,87 @@ type FxTokenClient struct {
 }
 
 func TokenInitialize(f FxTokenClientParam) (*FxTokenClient, error) {
+	email := f.Config.GetString("modules.stoken.email.username")
 	
-	f.Lifecycle.Append(fx.Hook{
-		OnStart: func(context.Context) error {
-			err := f.Factory.TokenInit()
+	apiBasePath := "/api/auth"
+	webBasePath := "/api/auth"
+	
+	err := supertokens.Init(
+		
+		supertokens.TypeInput{
 			
-			if err != nil {
-				return err
-			}
-			return nil
+			Supertokens: &supertokens.ConnectionInfo{
+				ConnectionURI: f.Config.GetString("modules.stoken.apiUrl"),
+				APIKey:        f.Config.GetString("modules.stoken.apiKey"),
+			},
+			
+			AppInfo: supertokens.AppInfo{
+				AppName:       "UTOL",
+				WebsiteDomain: "http://localhost:3000",
+				APIDomain:     "http://localhost:8080",
+				//APIDomain:       config.AppUrl(),
+				APIBasePath:     &apiBasePath,
+				WebsiteBasePath: &webBasePath,
+				APIGatewayPath:  nil,
+			},
+			
+			RecipeList: []supertokens.Recipe{
+				passwordless.Init(
+					plessmodels.TypeInput{
+						
+						FlowType: "USER_INPUT_CODE_AND_MAGIC_LINK",
+						ContactMethodEmailOrPhone: plessmodels.ContactMethodEmailOrPhoneConfig{
+							Enabled: true,
+						},
+						
+						//Override: &plessmodels.OverrideStruct{
+						//	Functions: OverRideSignIn,
+						//	APIs:      nil,
+						//},
+						
+						EmailDelivery: &emaildelivery.TypeInput{
+							
+							Service: passwordless.MakeSMTPService(emaildelivery.SMTPServiceConfig{
+								
+								Settings: emaildelivery.SMTPSettings{
+									Host: f.Config.GetString("modules.stoken.email.host"),
+									From: emaildelivery.SMTPFrom{
+										Name:  "OTP",
+										Email: email,
+									},
+									Port: 465,
+									//Username: &smtpUsername, // this is optional. In case not given, from.email will be used
+									Username: &email,
+									Password: f.Config.GetString("modules.stoken.email.password"),
+									Secure:   false,
+								},
+							}),
+						},
+					},
+				),
+			},
 		},
-	})
+	)
 	
-	return &FxTokenClient{
-		STokenInit: true,
-	}, nil
+	if err != nil {
+		return &FxTokenClient{STokenInit: false}, err
+	}
+	
+	return &FxTokenClient{STokenInit: true}, nil
+	
+	//f.Lifecycle.Append(fx.Hook{
+	//	OnStart: func(context.Context) error {
+	//		err := f.Factory.TokenInit()
+	//
+	//		if err != nil {
+	//			return err
+	//		}
+	//		return nil
+	//	},
+	//})
+	//
+	//return &FxTokenClient{
+	//	STokenInit: true,
+	//}, nil
 	
 }
