66129e17288e9cf74928dcac992fb38416d19e02
Jim Dela Cruz <dcruz.jim21@gmail.com>
2025-06-25 10:05:08 +0800

    fix: token intialization

--- module.go
+++ module.go
@@ -1,11 +1,9 @@
 package stoken
 
 import (
+	"context"
+	
 	"github.com/ankorstore/yokai/config"
-	"github.com/supertokens/supertokens-golang/ingredients/emaildelivery"
-	"github.com/supertokens/supertokens-golang/recipe/passwordless"
-	"github.com/supertokens/supertokens-golang/recipe/passwordless/plessmodels"
-	"github.com/supertokens/supertokens-golang/supertokens"
 	"go.uber.org/fx"
 )
 
@@ -14,7 +12,7 @@ const ModuleName = "stoken"
 var FxSTokenModule = fx.Module(
 	ModuleName,
 	fx.Provide(
-		//fx.Annotate(NewDefaultTokenClientFactory, fx.As(new(TokenClientFactory))),
+		fx.Annotate(NewDefaultTokenClientFactory, fx.As(new(TokenClientFactory))),
 		TokenInit,
 	),
 	fx.Invoke(func(*FxTokenClient) {}),
@@ -33,83 +31,86 @@ type FxTokenClient struct {
 
 func TokenInit(f FxTokenClientParam) (*FxTokenClient, error) {
 	
-	//err := f.Factory.TokenInit()
+	f.Lifecycle.Append(fx.Hook{
+		OnStart: func(context.Context) error {
+			err := f.Factory.TokenInit()
+			
+			if err != nil {
+				return err
+			}
+			return nil
+		},
+	})
+	
+	return &FxTokenClient{
+		STokenInit: true,
+	}, nil
+	
+	//email := f.Config.GetString("modules.stoken.email.username")
+	//
+	//apiBasePath := "/api/auth"
+	//webBasePath := "/api/auth"
+	//
+	//err := supertokens.Init(
+	//
+	//	supertokens.TypeInput{
+	//
+	//		Supertokens: &supertokens.ConnectionInfo{
+	//			ConnectionURI: f.Config.GetString("modules.stoken.apiUrl"),
+	//			APIKey:        f.Config.GetString("modules.stoken.apiKey"),
+	//		},
+	//
+	//		AppInfo: supertokens.AppInfo{
+	//			AppName:       "UTOL",
+	//			WebsiteDomain: "http://localhost:3000",
+	//			APIDomain:     "http://localhost:8080",
+	//			//APIDomain:       config.AppUrl(),
+	//			APIBasePath:     &apiBasePath,
+	//			WebsiteBasePath: &webBasePath,
+	//			APIGatewayPath:  nil,
+	//		},
+	//
+	//		RecipeList: []supertokens.Recipe{
+	//			passwordless.Init(
+	//				plessmodels.TypeInput{
+	//
+	//					FlowType: "USER_INPUT_CODE_AND_MAGIC_LINK",
+	//					ContactMethodEmailOrPhone: plessmodels.ContactMethodEmailOrPhoneConfig{
+	//						Enabled: true,
+	//					},
+	//
+	//					//Override: &plessmodels.OverrideStruct{
+	//					//	Functions: OverRideSignIn,
+	//					//	APIs:      nil,
+	//					//},
+	//
+	//					EmailDelivery: &emaildelivery.TypeInput{
+	//
+	//						Service: passwordless.MakeSMTPService(emaildelivery.SMTPServiceConfig{
+	//
+	//							Settings: emaildelivery.SMTPSettings{
+	//								Host: f.Config.GetString("modules.stoken.email.host"),
+	//								From: emaildelivery.SMTPFrom{
+	//									Name:  "OTP",
+	//									Email: email,
+	//								},
+	//								Port: 465,
+	//								//Username: &smtpUsername, // this is optional. In case not given, from.email will be used
+	//								Username: &email,
+	//								Password: f.Config.GetString("modules.stoken.email.password"),
+	//								Secure:   false,
+	//							},
+	//						}),
+	//					},
+	//				},
+	//			),
+	//		},
+	//	},
+	//)
 	//
 	//if err != nil {
-	//	return &FxTokenClient{
-	//		STokenInit: false,
-	//	}, err
+	//	return &FxTokenClient{STokenInit: false}, nil
 	//}
 	//
-	//return &FxTokenClient{
-	//	STokenInit: true,
-	//}, nil
-	
-	email := f.Config.GetString("modules.stoken.email.username")
-	
-	apiBasePath := "/api/auth"
-	webBasePath := "/api/auth"
-	
-	err := supertokens.Init(
-		
-		supertokens.TypeInput{
-			
-			Supertokens: &supertokens.ConnectionInfo{
-				ConnectionURI: f.Config.GetString("modules.stoken.apiUrl"),
-				APIKey:        f.Config.GetString("modules.stoken.apiKey"),
-			},
-			
-			AppInfo: supertokens.AppInfo{
-				AppName:       "UTOL",
-				WebsiteDomain: "http://localhost:3000",
-				APIDomain:     "http://localhost:8080",
-				//APIDomain:       config.AppUrl(),
-				APIBasePath:     &apiBasePath,
-				WebsiteBasePath: &webBasePath,
-				APIGatewayPath:  nil,
-			},
-			
-			RecipeList: []supertokens.Recipe{
-				passwordless.Init(
-					plessmodels.TypeInput{
-						
-						FlowType: "USER_INPUT_CODE_AND_MAGIC_LINK",
-						ContactMethodEmailOrPhone: plessmodels.ContactMethodEmailOrPhoneConfig{
-							Enabled: true,
-						},
-						
-						//Override: &plessmodels.OverrideStruct{
-						//	Functions: OverRideSignIn,
-						//	APIs:      nil,
-						//},
-						
-						EmailDelivery: &emaildelivery.TypeInput{
-							
-							Service: passwordless.MakeSMTPService(emaildelivery.SMTPServiceConfig{
-								
-								Settings: emaildelivery.SMTPSettings{
-									Host: f.Config.GetString("modules.stoken.email.host"),
-									From: emaildelivery.SMTPFrom{
-										Name:  "OTP",
-										Email: email,
-									},
-									Port: 465,
-									//Username: &smtpUsername, // this is optional. In case not given, from.email will be used
-									Username: &email,
-									Password: f.Config.GetString("modules.stoken.email.password"),
-									Secure:   false,
-								},
-							}),
-						},
-					},
-				),
-			},
-		},
-	)
-	
-	if err != nil {
-		return &FxTokenClient{STokenInit: false}, nil
-	}
-	
-	return &FxTokenClient{STokenInit: true}, nil
+	//return &FxTokenClient{STokenInit: true}, nil
 }
